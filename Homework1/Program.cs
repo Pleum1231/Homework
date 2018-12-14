01a6e633a60e145d22b3695506711aa517aacc65
Weerapat Singhawattanasiri <6005002578@rumail.ru.ac.th>
2018-12-15 01:03:20 +0700

    Gacha ruind my life.

---
+++ Homework1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using GuessNumberGame;
+namespace Homework1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string menu = "\nWelcome to program Guess Number\n" +
+                "this program work with 3 or 4 Arguments.\n" +
+                "###################################################################\n" +
+                "Argument 1 = select mode to play.(0=non-limit guess, 1=limit guess)\n" +
+                "Argument 2 = mix number to guess.\n" +
+                "Argument 3 = max number to guess.\n" +
+                "Argument 4(if Argument 1 = 1) = number of times to guess.\n" +
+                "###################################################################\n";
+
+            if(args.Length==4)
+            {
+                Game GuessNumber = new Game();
+                GuessNumber.Start(Convert.ToInt32(args[0]), Convert.ToInt32(args[1] ), Convert.ToInt32(args[2]), Convert.ToInt32(args[3]) );
+            }
+
+            else if(args.Length==3)
+            {
+                Game GuessNumber = new Game();
+                GuessNumber.Start(Convert.ToInt32(args[0]), Convert.ToInt32(args[1]), Convert.ToInt32(args[2]), 0);
+            }
+
+            else
+            {
+                Console.WriteLine("{0}", menu);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Homework1/SubProgram.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GuessNumberGame
+{
+    class RAND
+    {
+        public int RandomNumber(int min,int max)
+        {
+            Random random = new Random();
+            return random.Next(min,max);
+        }
+    }
+
+    class Game
+    {
+        RAND RObject = new RAND();
+        int count=0;
+        int num=0;
+        int guess=0;
+        public
+        void Start(int mode,int min,int max,int time)
+        {
+            num=RObject.RandomNumber(min, max);
+           
+            msg1(min,max);
+            if (mode == 0)
+            {
+                while (guess != num)
+                {
+                    msg5();
+                    int.TryParse(Console.ReadLine(), out guess);
+                    count++;
+                    if (num > guess)
+                    {
+                        msg3();
+                    }
+                    else if (num < guess)
+                    {
+                        msg2();
+                    }
+
+                }
+                msg4();
+                msg6();
+            }
+            else
+            {
+                while (guess != num&& count<time)
+                {
+                    msg5();
+                    int.TryParse(Console.ReadLine(), out guess);
+                    count++;
+                    if (num > guess)
+                    {
+                        msg3();
+                    }
+                    else if (num < guess)
+                    {
+                        msg2();
+                    }
+                    if(time-count>0)
+                    {
+                        Console.WriteLine("You have {0} times left.\n", time - count);
+                    }
+                }
+                if (count <= time && guess == num)
+                {
+                    msg4();
+                    msg6();
+                }
+                else
+                {
+                    msg7();
+                }
+            }
+        }
+        void msg1(int min,int max)
+        {
+            Console.WriteLine(" Welcome to GuessNumber game!");
+            Console.WriteLine("min number = {0}  max number = {1}\n", min, max);
+
+        }
+        void msg2()
+        {
+            Console.WriteLine("That's wrong! Your Guess number are greater than Random number\n");
+        }
+        void msg3()
+        {
+            Console.WriteLine("That's wrong! Your Guess number are less than Random number\n");
+        }
+        void msg4()
+        {
+            Console.WriteLine("That's correct, You win!");
+        }
+        void msg5()
+        {
+            Console.Write("Number ? ");
+        }
+        void msg6()
+        {
+            Console.Write("You guess {0} times.",count);
+        }
+        void msg7()
+        {
+            Console.WriteLine("You out of time to guess, you loss...\n" +
+                "Try again next time!");
+        }
+        
+    }
+}
